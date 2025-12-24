6756018004d9f0586f32447ed08ffbb9d6c2b098
Potapenko Max <mpotapenko.mail@gmail.com>
2025-12-24 16:25:29 +0300

    feat/textures

--- include/config/AudioConfig.hpp
+++ include/config/AudioConfig.hpp
@@ -2,15 +2,8 @@
 #include <string>
 
 // SFX
-constexpr const char *SFX_SHOOT = "assets/audio/sfx/shoot.wav";
-constexpr const char *SFX_EXPLOSION = "assets/audio/sfx/explosion.wav";
-constexpr const char *SFX_POWERUP = "assets/audio/sfx/powerup.wav";
-constexpr const char *SFX_PLAYER_HIT = "assets/audio/sfx/player_hit.wav";
-constexpr const char *SFX_BOSS_LASER = "assets/audio/sfx/boss_laser.wav";
-constexpr float SFX_VOLUME = 100.f;
+constexpr auto AUDIO_SFX_SHOOT = "assets/audio/sfx/shoot.wav";
+constexpr float AUDIO_SFX_VOLUME = 100.f;
 
 // music
-constexpr const char *MUSIC_MENU = "assets/audio/music/menu.ogg";
-constexpr const char *MUSIC_GAMEPLAY = "assets/audio/music/gameplay.ogg";
-constexpr const char *MUSIC_BOSS = "assets/audio/music/boss.ogg";
-constexpr float MUSIC_VOLUME = 50.f;
+constexpr float AUDIO_MUSIC_VOLUME = 50.f;
